46ed20c083d0afd0abd52d61d8077d42ce8c9568
ibhoomi <119471440+ibhoomi@users.noreply.github.com>
2022-12-07 20:02:52 +0530

    Add files via upload

---
+++ python.p greatest of three.py
@@ -0,0 +1,11 @@
+a=int(input ("enter your first number :" ))
+
+b=int( input("enter your second number:")) 
+
+c=int(input( "enter your third number:"))
+
+if a>b and a>c :
+ print(" a" )             
+if b>c and b>a :           
+ print ( "b")
+else : print("c")
